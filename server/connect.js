9ba5eaabe51c793903831530f3c6eb49dbb5db64
open-dream-studios <opendreamstudios@gmail.com>
2025-03-02 15:28:02 -0500

    fixed env for server

--- server/connect.js
+++ server/connect.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createConnection({
-    host: 'switchback.proxy.rlwy.net',
-    port: 21464,
-    user: 'root',
-    password: 'ayBSorwPhuHThXBFXLtLsUkyowyCpwjt',
-    database: 'ace-interview' 
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA
 });
